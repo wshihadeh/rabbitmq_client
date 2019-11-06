21540520f9c698f5dcb29c451e9cc3b5184cfdec
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-06 21:36:12 +0000

    Deploy wshihadeh/rabbitmq_client to github.com/wshihadeh/rabbitmq_client.git:gh-pages

--- script/travis.sh
+++ script/travis.sh
@@ -13,8 +13,6 @@ if [ -z "$QUICK" ]; then
     echo "Pushing badges upstream"
     [ -d badges ] || mkdir badges
     cp coverage/coverage_badge* badges/ 2>/dev/null || true
-    cp -r coverage coverage_info 2>/dev/null || true
-    cp -r tmp/rubycritic rubycritic 2>/dev/null || true
   fi
 
 fi
--- vendor/bundle/ruby/2.6.0/gems/reverse_markdown-1.3.0/lib/reverse_markdown/version.rb
+++
@@ -1,3 +0,0 @@
-module ReverseMarkdown
-  VERSION = '1.3.0'
-end
